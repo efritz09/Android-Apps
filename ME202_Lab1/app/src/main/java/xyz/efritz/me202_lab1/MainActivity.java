19367d808e2b6ca897138bd507851c32e1924086
Eric Fritz <efritz09@gmail.com>
2016-04-03 17:53:36 -0700

    Initial push, lab1
    Includes some other crap that I'll eventually delete

---
+++ ME202_Lab1/app/src/main/java/xyz/efritz/me202_lab1/MainActivity.java
@@ -0,0 +1,39 @@
+package xyz.efritz.me202_lab1;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.nio.charset.Charset;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    String testcode = "test";
+
+    public void login_press(View view) {
+        EditText e_username = (EditText)findViewById(R.id.username_text);
+        EditText e_password = (EditText)findViewById(R.id.password_text);
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        if( e_username.getText().toString().equals(testcode) && e_password.getText().toString().equals(testcode)) {
+            CharSequence text = "Login Succeeded";
+            Toast login_success = Toast.makeText(context, text, duration);
+            login_success.show();
+        }else {
+            CharSequence text = e_username.getText().toString() + e_password.getText().toString();
+            Toast login_failed = Toast.makeText(context, text, duration);
+            login_failed.show();
+        }
+    }
+
+}
